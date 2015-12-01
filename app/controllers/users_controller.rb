bb0b7e22fb241ceacff18dd8ddeb17579af9d028
Eder Freire <ederssfreire@gmail.com>
2015-12-01 09:22:55 -0200

    added CRUD in User module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params) 
+			redirect_to @user, :notice => 'Cadastro atualizado com sucesso'
+		else
+			render :edit
+		end
+	end
+
     def user_params
       params.require(:user).permit(:email, :full_name, :confirmation_password, :password, :location, :bio)
     end
